d04ce7c787b516dc88b998d1042bae38311a7a39
Alessio <slux83@gmail.com>
2022-10-10 22:34:29 +0200

    some operational fixes

--- scripts/start.sh
+++ scripts/start.sh
@@ -7,15 +7,17 @@ DB_USER="postgres"
 DB_PASSWORD="postgres"
 DB_HOST="localhost"
 DB_PORT="5432"
-DB_NAME="thoth_dev"
+DB_NAME="thoth"
 THOTH_VERSION="1.0.0-beta"
 LOGS_FOLDER="${DIR}/logs"
+
+echo "TOKEN: ${TELEGRAM_BOT_TOKEN_ENV}"
 
 mkdir -p "${LOGS_FOLDER}"
 
 echo "*** Starting THOTH BOT v${THOTH_VERSION} ***"
 
-java -jar target/thoth-bot-${THOTH_VERSION}.jar \
+java -jar ${DIR}/../target/thoth-bot-${THOTH_VERSION}.jar \
       ${JAVA_OPTS} \
       --thoth.test-mode=false \
       --logging.level.com.devpool=DEBUG \
@@ -23,10 +25,5 @@ java -jar target/thoth-bot-${THOTH_VERSION}.jar \
       --telegram.bot.token="${TELEGRAM_BOT_TOKEN}" \
       --spring.datasource.username=${DB_USER} \
       --spring.datasource.password=${DB_PASSWORD} \
-      --spring.datasource.url=jdbc:postgresql://${DB_HOST}:${DB_PORT}/${DB_NAME} > /dev/null 2>&1 &
+      --spring.datasource.url=jdbc:postgresql://${DB_HOST}:${DB_PORT}/${DB_NAME} 2>&1
 
-# get the app pid
-rm -f "${DIR}/THOTH.pid"
-echo $! > "${DIR}/THOTH.pid"
-
-echo "*** THOTH BOT v${THOTH_VERSION} started with PID $(cat "${DIR}/THOTH.pid") ***"
