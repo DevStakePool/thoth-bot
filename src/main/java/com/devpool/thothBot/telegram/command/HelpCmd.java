b267d2998f9028521b412956caa1fec5fe199ec1
Alessio <slux83@gmail.com>
2025-03-09 13:56:49 +0100

    Moved to version 2.0.0
    Signed-off-by: Alessio <slux83@gmail.com>

--- src/main/java/com/devpool/thothBot/telegram/command/HelpCmd.java
+++ src/main/java/com/devpool/thothBot/telegram/command/HelpCmd.java
@@ -34,7 +34,7 @@ public class HelpCmd implements IBotCommand {
      * Some global constants
      */
     public static final Map<String, String> CONSTANTS = Map.of(
-            "$thoth.version", "1.9.2",
+            "$thoth.version", "2.0.0",
             "$donation.handle", "$thoth-bot",
             "$url", "https://github.com/DevStakePool/thoth-bot");
 
